25c52e333a59b6d062f0c4edc950205147a186d5
Ben Hughes <ben@railsgarden.com>
2009-04-02 21:42:18 -0400

    Cleared out older test files and changed to Rails 2.3.2.

--- app/controllers/application.rb
+++
@@ -1,3 +0,0 @@
-class ApplicationController < ActionController::Base #:nodoc:
-  protect_from_forgery :secret => 'ee61169c15b3e4922527a08350a7932a'
-end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,2 @@
+class ApplicationController < ActionController::Base #:nodoc:
+end
